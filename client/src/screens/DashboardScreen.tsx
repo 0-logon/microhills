af7a83f616590c1fc72ff8cb84f6969a5ed20859
markotomic <masheritto@gmail.com>
2024-12-05 23:29:34 +0100

    Some fixes

--- client/src/screens/DashboardScreen.tsx
+++ client/src/screens/DashboardScreen.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import styles from '../styles/screens/DashboardScreen.module.css';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store';
 import ActivityWidget from '../components/widgets/ActivityWidget';
 import QuoteWidget from '../components/widgets/UsageWidget';
 
 const DashboardScreen: React.FC = () => {
-    const { tasks, status, error, charCount } = useSelector((state: RootState) => state.tasks);
-
-    console.log("Tasks from redux: ", charCount);
     return (
         <div className={styles.component}>
             <div className={styles.header}>
